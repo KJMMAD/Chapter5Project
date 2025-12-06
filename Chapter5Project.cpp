d33702602231ef0f4a7d6221ea6be6837db0aaf6
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-12-06 13:52:15 -0500

    Added some code

--- Chapter5Project.cpp
+++ Chapter5Project.cpp
@@ -1,38 +1,8 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-	int pStart, pFinal,t; //t=days
-	float incRate;
-	cout << "How many organisms are in the population to start with?";
-	//pFinal = pStart(e)^rt for population growth
-	cin >> pStart;
-	if (pStart < 2) {
-		cout << "The population must be greater or equal to 2.";
-		return 0;
-	}
-
-	cout << "At what rate will the population increase?";
-	cin >> incRate;
-	if (incRate <= 0) {
-		cout << "This program only calculates an increase in population, please select a number greater than 0";
-		return 0;
-	}
-
-	cout << "How many days will the organisms multiply?";
-	cin >> t;
-	if (t < 1) {
-		cout << "The number of days must be greater than 1.";
-		return 0;
-	}
-
-	for (int i = 1; i <= t; i++) {
-		cout << "Population for day " << i;
-		pFinal = pStart * exp(incRate * i);
-		cout << " " << pFinal << "\n";
-	}
-	float Ptest = pStart * exp(incRate * t);
-	cout << Ptest;
+	int max;
+	cout << "Please enter a number greater than 1.";
 }
