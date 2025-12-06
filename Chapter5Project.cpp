749b35b09898c745aaab1f7179e2feaac627e7f3
KJMMAD <keira_jmmadrid.@yahoo.com>
2025-12-06 14:23:41 -0500

    Finished code for patterns program.

--- Chapter5Project.cpp
+++ Chapter5Project.cpp
@@ -4,5 +4,23 @@ using namespace std;
 int main()
 {
 	int max;
+	string patternA;
 	cout << "Please enter a number greater than 1.";
+	cin >> max;
+
+	cout << "Pattern A" << endl;
+
+	for (int i = 0; i < max; i++) {
+		patternA += '+';
+		cout << patternA << endl;
+	}
+	cout << "Pattern B" << endl;
+
+	for (int i = max; i > 0; i--) {
+		string patternB;
+		for (int b = 0; b < i; b++) {
+			patternB += '+';
+		}
+		cout << patternB << endl;
+	}
 }
